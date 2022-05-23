7d58982c0076e7d3b89db9d78f036c2762069490
formateur1 <enseignant.formateur@protonmail.com>
2022-05-23 11:12:59 +0200

    Ajout des annotaitons dans la classe Role

--- src/main/java/com/intiformation/SpringBootThymeLeaf/model/Role.java
+++ src/main/java/com/intiformation/SpringBootThymeLeaf/model/Role.java
@@ -1,6 +1,18 @@
 package com.intiformation.SpringBootThymeLeaf.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class Role
 {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String nomRole;
 
 }
